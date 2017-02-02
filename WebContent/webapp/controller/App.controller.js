5f5331197b2ad7664c25d95febead1704ddc6aa9
Alexander Seibel <a.seibel90@gmx.de>
2017-02-02 13:22:11 +0100

    Initil project commit

---
+++ WebContent/webapp/controller/App.controller.js
@@ -0,0 +1,29 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageBox"
+], function(Controller, MessageBox) {
+	"use strict";
+
+	return Controller.extend("lan.party.tournament.controller.App", {
+	  
+	  onPress: function(){
+	    
+	    console.log(this.getView().getModel("Gamers").getJSON());
+	    var data = this.getView().getModel("Gamers").getJSON();
+	    $.ajax({
+	      type: "POST",
+	      url: "../../../../php/postGamerData.php",
+	      data: data,
+	      success: function(data, response){
+	        MessageBox.success("Gamers updated");
+	      },
+	      error: function(response){
+	        MessageBox.error(response.responseText);
+	      },
+	      dataType: "json"
+	    });
+	  }
+
+	});
+
+});
